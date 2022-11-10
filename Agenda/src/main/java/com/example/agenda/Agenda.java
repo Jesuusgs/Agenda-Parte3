42fbd60e637ff70c4dcbfd2da403aeeb793f98d3
Jesuusgs <98019367+Jesuusgs@users.noreply.github.com>
2022-11-10 13:56:04 +0100

    Parte 4
    Realizado por Jesús García Segovia el 10/11/2022 a las 13.55h

--- Agenda/src/main/java/com/example/agenda/Agenda.java
+++ Agenda/src/main/java/com/example/agenda/Agenda.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -43,6 +44,9 @@ public class Agenda extends Application{
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Agenda");
+
+        //añadir un icono a la aplicación
+        this.primaryStage.getIcons().add(new Image("file:images/agenda.png"));
 
         initRootLayout();
 
--- Agenda/src/main/java/com/example/agenda/PersonEditDialogController.java
+++ Agenda/src/main/java/com/example/agenda/PersonEditDialogController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -33,6 +34,7 @@ public class PersonEditDialogController {
 
     @FXML
     private void initialize() {
+
     }
 
     public void setDialogStage(Stage dialogStage) {
